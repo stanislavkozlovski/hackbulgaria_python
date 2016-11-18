7faf51c2ee51d8dee6e8eca896193abb6695673a
Enether <familyguyuser192@windowslive.com>
2016-11-18 18:17:06 +0200

    Added find_turning_point using binary search

--- week05/03/binary_search.py
+++ week05/03/binary_search.py
@@ -1,18 +1,18 @@
-arr = [1,2,3,4,5,6,7,8,9,10]
-
-def binary_search(arr, start, end, element):
-    mid = (start+end) // 2
-    mid_el = arr[mid]
-
-    if mid_el == element:
-        return mid
-
-    if mid_el > element:
-        return binary_search(arr, start, mid, element)
-    else:
-        return binary_search(arr, mid, end, element)
-
-print(binary_search(arr, 0, 10, element=9))
+# arr = [1,2,3,4,5,6,7,8,9,10]
+#
+# def binary_search(arr, start, end, element):
+#     mid = (start+end) // 2
+#     mid_el = arr[mid]
+#
+#     if mid_el == element:
+#         return mid
+#
+#     if mid_el > element:
+#         return binary_search(arr, start, mid, element)
+#     else:
+#         return binary_search(arr, mid, end, element)
+#
+# print(binary_search(arr, 0, 10, element=9))
 
 """
 Find a point where arrays starts decreasing, array is first increasing and then decreasing.
@@ -25,5 +25,24 @@ For example if we have following array [1, 3, 7, 9, 4, 2], the turning point is 
 """
 
 
-def find_turning_point(array, start, end):
-    pass
+def find_turning_point(arr, start, end):
+    mid = (start+end) // 2
+    mid_el = arr[mid]
+
+    if (mid - 1 >= 0 and mid + 1 < len(arr)
+        and arr[mid-1] < mid_el and arr[mid+1] < mid_el):
+        return mid  # turning point
+    elif mid - 1 < 0 and mid_el > arr[mid+1]:
+        return mid  # turning point at start - [4,1,2]
+    elif mid + 1 >= len(arr) and mid_el < arr[mid-1]:
+        return mid  # turning point at end - [1,4,3]
+
+    if mid-1 >= 0 and mid_el > arr[mid-1]:  # still increasing, so search right
+        return find_turning_point(arr, mid, end)
+
+    if mid-1 >= 0 and mid_el < arr[mid-1]: # still decreasing, so search left
+        return find_turning_point(arr, start, mid)
+
+
+test = [1,4,3]
+print(find_turning_point(test, 0, len(test)))
