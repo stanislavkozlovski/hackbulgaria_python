5a3166c0bf7eccaab54d8434cb877f59a37db9db
Enether <familyguyuser192@windowslive.com>
2016-12-03 20:49:50 +0200

    Updated tests for the music_crawler.py file

--- week07/01/tests_music_crawler.py
+++ week07/01/tests_music_crawler.py
@@ -3,6 +3,7 @@ import os
 from datetime import timedelta
 from music_crawler import MusicCrawler
 
+
 class MusicCrawlerTests(unittest.TestCase):
 
     def setUp(self):
@@ -17,14 +18,18 @@ class MusicCrawlerTests(unittest.TestCase):
 
     def test_generate_playlist(self):
         crawler = MusicCrawler(self.songs_path)
-        playlist = crawler.generate_playlist()  # should have only one song
-        song_DRAKE_WORST_BEHAVIOUR = playlist.next_song()
-        self.assertEqual(song_DRAKE_WORST_BEHAVIOUR.title, 'Worst Behavior')
-        self.assertEqual(song_DRAKE_WORST_BEHAVIOUR.artist, 'Drake')
-        self.assertEqual(song_DRAKE_WORST_BEHAVIOUR.album, 'Nothing Was The Same')
-        self.assertEqual(song_DRAKE_WORST_BEHAVIOUR.length, '4:37')
-        self.assertEqual(song_DRAKE_WORST_BEHAVIOUR.timedelta_length, timedelta(hours=0, minutes=4, seconds=37))
-        self.assertEqual(str(song_DRAKE_WORST_BEHAVIOUR), 'Drake - Worst Behavior from Nothing Was The Same - 4:37')
+        # should have only two identical songs from different formats - .ogg & .mp3
+        playlist = crawler.generate_playlist()
+        song_count = 2
+        for idx in range(song_count):
+            with self.subTest(song_idx=idx):
+                song_DRAKE_WORST_BEHAVIOUR = playlist.next_song()
+                self.assertEqual(song_DRAKE_WORST_BEHAVIOUR.title, 'Worst Behavior')
+                self.assertEqual(song_DRAKE_WORST_BEHAVIOUR.artist, 'Drake')
+                self.assertEqual(song_DRAKE_WORST_BEHAVIOUR.album, 'Nothing Was The Same')
+                self.assertEqual(song_DRAKE_WORST_BEHAVIOUR.length, '4:37')
+                self.assertEqual(song_DRAKE_WORST_BEHAVIOUR.timedelta_length, timedelta(hours=0, minutes=4, seconds=37))
+                self.assertEqual(str(song_DRAKE_WORST_BEHAVIOUR), 'Drake - Worst Behavior from Nothing Was The Same - 4:37')
 
 if __name__ == '__main__':
     unittest.main()
