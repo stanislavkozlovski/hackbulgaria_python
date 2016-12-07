364740600b323b5d4dcbc59cc9f524398d553b92
Enether <familyguyuser192@windowslive.com>
2016-12-07 18:46:06 +0200

    Revert "Modified some .sql queries from week08/01"
    This reverts commit a3c328eed713a6f691651eadc4cfbedb769a1fcf.

--- week08/01/relations.sql
+++ week08/01/relations.sql
@@ -1,49 +1,23 @@
-/*Напишете заявка, която за всеки филм, по-дълъг от 120 минути, извежда заглавие, година, име и адрес на студио.*/
-SELECT movie.title, movie.year, studio.name, studio.address
-FROM movie
-INNER JOIN studio
-ON movie.length > 120 AND movie.studioname = studio.name;
-/*
-Напишете заявка, която извежда името на студиото и имената на актьорите, участвали във филми, произведени от това студио, подредени по име на студио.*/
-SELECT movie.studioname, starsin.starname
+/*Напишете заявка, която извежда имената на актьорите мъже участвали в ‘Terms
+of Endearment*/
+SELECT starsin.starname
 FROM starsin
-INNER JOIN movie
-ON movie.title = starsin.movietitle
-ORDER BY movie.studioname
-/*Напишете заявка, която извежда имената на продуцентите на филмите, в които е играл Harrison Ford.*/
+WHERE starsin.movietitle == "Terms of Endearment";
+/*Напишете заявка, която извежда имената на актьорите участвали във филми продуцирани от ‘MGM’през 1995 г.*/
+SELECT starsin.starname
+FROM starsin, movie
+WHERE starsin.movietitle == movie.title AND movie.studioname = "MGM" AND movie.year = 1995;
+/*Напишете заявка, която извежда името на президента на ‘MGM’*/
+SELECT DISTINCT movieexec.name
+FROM movieexec, studio, movie
+WHERE studio.name == movie.studioname AND movieexec.cert == movie.producer;
+/*Напишете заявка, която извежда имената на всички филми с дължина по-голяма от дължината на филма ‘Gone With the Wind’*/
+SELECT *
+FROM movie
+WHERE movie.length > (
+SELECT movie.length FROM movie WHERE movie.title = "Gone With the Wind");
+/*Напишете заявка, която извежда имената на тези продукции на стойност по- голяма от ‘Mery Griffin’*/
 SELECT movieexec.name
 FROM movieexec
-WHERE movieexec.cert = 
-	(SELECT movie.producer
-	FROM movie
-	INNER JOIN
-		(SELECT starsin.movietitle, starsin.movietitle
-		FROM starsin
-		WHERE starname="Harrison Ford") as hfMovie
-	ON hfMovie.movietitle = movie.title);
-/*Напишете заявка, която извежда имената на актрисите, играли във филми на MGM*/
-SELECT moviestar.name
-FROM moviestar
-INNER JOIN 
-	(SELECT starsin.starname
-	FROM starsin
-	INNER JOIN
-		(SELECT movie.title
-		FROM movie
-		WHERE movie.studioname = "MGM") as mgmMovie
-	ON mgmMovie.title = starsin.movietitle) as actor
-ON actor.starname = moviestar.name and moviestar.gender = "F";
-/*Напишете заявка, която извежда името на продуцента и имената на филмите, продуцирани от продуцента на ‘Star Wars’.*/
-SELECT producer.name, movie.title
-FROM movie
-INNER JOIN
-	(SELECT movieexec.name, movieexec.cert
-	FROM movieexec
-	INNER JOIN movie
-	ON movie.title = "Star Wars" AND movie.producer = movieexec.cert) as producer
-WHERE movie.producer = producer.cert;
-/*Напишете заявка, която извежда имената на актьорите не участвали в нито един филм*/
-SELECT moviestar.name
-FROM moviestar
-LEFT JOIN starsin ON starsin.starname = moviestar.name
-WHERE starsin.starname IS NULL;
+WHERE movieexec.networth >
+(SELECT movieexec.networth FROM movieexec WHERE movieexec.name = "Merv Griffin");
