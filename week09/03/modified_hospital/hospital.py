2cefdd7788451531ff76e848774a18e8fb3a3995
Enether <familyguyuser192@windowslive.com>
2016-12-16 17:38:31 +0200

    Completed the logic for registering a user

--- week09/03/modified_hospital/hospital.py
+++ week09/03/modified_hospital/hospital.py
@@ -1,7 +1,13 @@
 import sqlite3
-from settings import DB_NAME
 import queries
 import sys
+import getpass
+import hashlib, uuid
+
+
+from settings import DB_NAME, DOCTOR_TITLE
+from validator import validate_password
+
 
 connection = sqlite3.connect(DB_NAME)
 cursor = connection.cursor()
@@ -25,8 +31,34 @@ def create_tables(drop_old=False):
 
 def register_user():
     username = input(">Username\n")
-    password = input(">Password\n")
-    confirm_password = input(">Confirm Password\n")
+    password = getpass.getpass(prompt=">Password\n")
+
+    while not validate_password(password):
+        print("Your password must be longer than 7 characters "
+              "and must contain one uppercase character, one lowercase and one digit!")
+        password = getpass.getpass(prompt=">Password\n")
+
+    confirm_password = getpass.getpass(prompt=">Password\n")
+
+    while password != confirm_password:
+        print("Password do not match!")
+        confirm_password = getpass.getpass(prompt=">Confirm Password\n")
+
+    age = input(">Your age:\n")
+    while not age.isdigit():
+        print("Invalid age!")
+        age = input(">Your age:\n")
+
+    age = int(age)
+    # create a salt for the user and hash his password
+    user_salt = uuid.uuid4().hex
+    hashed_password = hashlib.sha512(password + user_salt).hexdigest()
+    user = (username, hashed_password, user_salt, age)
+
+    if DOCTOR_TITLE in username:
+        return register_doctor(user)
+    else:
+        return register_patient(user)
 
 
 def main():
@@ -40,12 +72,11 @@ Choose:
     if command == '1':
         pass
     elif command == '2':
-        pass
+        register_user()
     elif command == '3':
         pass
     elif command == '4':
         sys.exit()
-
 
 
 if __name__ == '__main__':
--- week09/03/modified_hospital/settings.py
+++ week09/03/modified_hospital/settings.py
@@ -1,2 +1,3 @@
 DB_NAME = 'modified_hospital.db'
+DOCTOR_TITLE = 'Dr.'
 DOCTOR_RANKS = ['student', 'intern', 'apprentice', 'journeyman', 'experienced', 'master']
