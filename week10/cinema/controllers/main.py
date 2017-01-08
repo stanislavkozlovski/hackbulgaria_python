7ce00c9eb995b7ae40d7b0bbab7272dd52cc0ee9
Enether <familyguyuser192@windowslive.com>
2017-01-08 23:33:54 +0200

    Made the make_reservations function use a decorator for authentication

--- week10/cinema/controllers/main.py
+++ week10/cinema/controllers/main.py
@@ -5,6 +5,7 @@ from settings.constants import (DB_ID_KEY, DB_MOVIE_NAME_KEY, DB_MOVIE_RATING_KE
                                 DB_PROJECTIONS_DATE_KEY, DB_PROJECTIONS_HOUR_KEY,
                                 DB_PROJECTIONS_MOVIE_TYPE_KEY, MOVIE_HALL_CAPACITY, DB_USERS_USERNAME_KEY)
 from settings.utils import get_free_spot_count_for_a_projection, get_free_spots_for_a_projection, print_movie_hall
+from settings.decorators import authenticate
 from queries.loader import (get_all_movies_ordered_by_date, get_movie_by_id, get_movie_projections_ordered_by_date,
                             get_reservations_by_projection_id, get_user_by_username_and_password)
 from queries.inserter import create_reservations
@@ -54,88 +55,7 @@ def read_spell(cinema: Cinema):
         movie, projections = get_movie_projections(movie_id, date)
         show_movie_projections(movie, projections, date)
     elif 'make reservation' in user_input:
-        if not cinema.has_logged_user():
-            cinema.log_user_in()
-            if not cinema.has_logged_user():
-                # If the user has declined logging in
-                print('You have to be logged in to make a reservation!')
-                return
-        print('Choose the amount of tickets you want:')
-        ticket_count = input(">Ticket count: ")
-        while not is_valid_ticket_count(ticket_count):
-            print('Invalid ticket count! The ticket count should be between 1-10 inclusive.')
-            ticket_count = input(">Ticket count: ")
-        ticket_count = int(ticket_count)
-        # show the movies and let the user choose one
-        show_movies()
-        movie_id = input(">Choose a movie: ")
-        movie = get_movie_by_id(movie_id)
-        while not movie:
-            print("Invalid movie id!")
-            movie_id = input(">Choose a movie: ")
-            movie = get_movie_by_id(movie_id)
-
-        # show the projections for the movie
-        movie, projections = get_movie_projections(movie_id)
-        if len(projections) == 0:
-            print('There are no projections for that movie, we apologize for the inconvenience.')
-            return
-        projections_by_id = {str(projection[DB_ID_KEY]): projection for projection in projections}
-        while True:
-            show_movie_projections(movie, projections)
-            projection_id = input(">Choose a projection: ")
-            if projection_id in projections_by_id:
-                # see if there are enough free spots
-                free_spots_count = get_free_spot_count_for_a_projection(projection_id)
-
-                if free_spots_count < ticket_count:
-                    print('There are not enough free spots for {} tickets!'.format(ticket_count))
-                    print('Please choose another projection')
-                    continue
-
-                break
-            print('Invalid projection id!')
-        projection = projections_by_id[projection_id]
-        movie_hall = get_free_spots_for_a_projection(projection_id)
-        tickets = {}  # type: {int:Ticket}
-        for i in range(ticket_count):
-            # loop until the user chooses a valid ticket
-            while True:
-                print("Please pick a spot for ticket #{}".format(i+1))
-                print_movie_hall(movie_hall)
-                row = input("Choose a row: (1-10): ")
-                while not is_valid_row_or_col(row):
-                    print('The row you entered is invalid.')
-                    row = input("Choose a row: (1-10): ")
-                row = int(row)
-                col = input("Choose a cow: (1-10): ")
-                while not is_valid_row_or_col(col):
-                    print('The col you entered is invalid.')
-                    col = input("Choose a col: (1-10): ")
-                col = int(col)
-                if movie_hall[row][col] == 'X':
-                    print('The spot you choose is taken!')
-                else:
-                    # take the seat and add the ticket
-                    movie_hall[row][col] = 'X'
-                    ticket = Ticket(row, col, movie_name=movie[DB_MOVIE_NAME_KEY], projection_id=projection[DB_ID_KEY],
-                                    proj_type=projection[DB_PROJECTIONS_MOVIE_TYPE_KEY],
-                                    date=projection[DB_PROJECTIONS_DATE_KEY], hour=projection[DB_PROJECTIONS_HOUR_KEY],
-                                    owner_id=cinema.user[DB_ID_KEY])
-                    tickets[i+1] = ticket
-                    break
-        # finalize order
-        print('You have chosen to create {reservation_count} reservations for the movie {movie_name} on {date}{time}'.format(
-            reservation_count=ticket_count, movie_name=movie[DB_MOVIE_NAME_KEY],
-            date=projection[DB_PROJECTIONS_DATE_KEY], time=projection[DB_PROJECTIONS_HOUR_KEY]
-        ))
-        for idx, ticket in tickets.items():
-            print("#{} {}".format(str(idx), ticket))
-
-        choice = input('Do you confirm your order? (y/n): ')
-        if choice in ['y', 'Y', 'yes', 'YES', 'Yes']:
-            create_reservations(tickets.values())
-
+        make_reservation(cinema)
 
 
 def show_movies():
@@ -184,6 +104,86 @@ def get_movie_projections(movie_id, date=None) -> tuple:
     return movie, get_movie_projections_ordered_by_date(movie[DB_ID_KEY], date)
 
 
+@authenticate
+def make_reservation(cinema: Cinema):
+    print('Choose the amount of tickets you want:')
+    ticket_count = input(">Ticket count: ")
+    while not is_valid_ticket_count(ticket_count):
+        print('Invalid ticket count! The ticket count should be between 1-10 inclusive.')
+        ticket_count = input(">Ticket count: ")
+    ticket_count = int(ticket_count)
+    # show the movies and let the user choose one
+    show_movies()
+    movie_id = input(">Choose a movie: ")
+    movie = get_movie_by_id(movie_id)
+    while not movie:
+        print("Invalid movie id!")
+        movie_id = input(">Choose a movie: ")
+        movie = get_movie_by_id(movie_id)
+
+    # show the projections for the movie
+    movie, projections = get_movie_projections(movie_id)
+    if len(projections) == 0:
+        print('There are no projections for that movie, we apologize for the inconvenience.')
+        return
+    projections_by_id = {str(projection[DB_ID_KEY]): projection for projection in projections}
+    while True:
+        show_movie_projections(movie, projections)
+        projection_id = input(">Choose a projection: ")
+        if projection_id in projections_by_id:
+            # see if there are enough free spots
+            free_spots_count = get_free_spot_count_for_a_projection(projection_id)
+
+            if free_spots_count < ticket_count:
+                print('There are not enough free spots for {} tickets!'.format(ticket_count))
+                print('Please choose another projection')
+                continue
+
+            break
+        print('Invalid projection id!')
+    projection = projections_by_id[projection_id]
+    movie_hall = get_free_spots_for_a_projection(projection_id)
+    tickets = {}  # type: {int:Ticket}
+    for i in range(ticket_count):
+        # loop until the user chooses a valid ticket
+        while True:
+            print("Please pick a spot for ticket #{}".format(i + 1))
+            print_movie_hall(movie_hall)
+            row = input("Choose a row: (1-10): ")
+            while not is_valid_row_or_col(row):
+                print('The row you entered is invalid.')
+                row = input("Choose a row: (1-10): ")
+            row = int(row)
+            col = input("Choose a cow: (1-10): ")
+            while not is_valid_row_or_col(col):
+                print('The col you entered is invalid.')
+                col = input("Choose a col: (1-10): ")
+            col = int(col)
+            if movie_hall[row][col] == 'X':
+                print('The spot you choose is taken!')
+            else:
+                # take the seat and add the ticket
+                movie_hall[row][col] = 'X'
+                ticket = Ticket(row, col, movie_name=movie[DB_MOVIE_NAME_KEY], projection_id=projection[DB_ID_KEY],
+                                proj_type=projection[DB_PROJECTIONS_MOVIE_TYPE_KEY],
+                                date=projection[DB_PROJECTIONS_DATE_KEY], hour=projection[DB_PROJECTIONS_HOUR_KEY],
+                                owner_id=cinema.user[DB_ID_KEY])
+                tickets[i + 1] = ticket
+                break
+    # finalize order
+    print(
+        'You have chosen to create {reservation_count} reservations for the movie {movie_name} on {date}{time}'.format(
+            reservation_count=ticket_count, movie_name=movie[DB_MOVIE_NAME_KEY],
+            date=projection[DB_PROJECTIONS_DATE_KEY], time=projection[DB_PROJECTIONS_HOUR_KEY]
+        ))
+    for idx, ticket in tickets.items():
+        print("#{} {}".format(str(idx), ticket))
+
+    choice = input('Do you confirm your order? (y/n): ')
+    if choice in ['y', 'Y', 'yes', 'YES', 'Yes']:
+        create_reservations(tickets.values())
+
+
 def log_user():
     """ Log in a user to the system"""
     print('Please log in:')
@@ -203,3 +203,6 @@ def log_user():
         user = get_user_by_username_and_password(username, password)
 
     return user
+cn = Cinema()
+while True:
+    read_spell(cn)
