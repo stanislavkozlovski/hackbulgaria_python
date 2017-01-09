950d18393b7e1f6faa9aa8faf162ae5370c38bd1
Enether <familyguyuser192@windowslive.com>
2017-01-09 11:33:10 +0200

    Added a decorator function for authenticating a user

--- week10/cinema/settings/decorators.py
+++ week10/cinema/settings/decorators.py
@@ -1,3 +1,6 @@
+from settings.constants import DB_USERS_USERNAME_KEY
+
+
 def authenticate(func):
     """
     A function used to authenticate (log in) the user into the system. Requires that a Cinema object is passed
@@ -13,3 +16,18 @@ def authenticate(func):
                 return
         func(*args, **kwargs)
     return _authenticate
+
+
+def authenticate_user(func):
+    """ A function used to authenticate (log in) the SPECIFIC user into the system. Requires that a Cinema object and
+     a username is passed to the decorated function."""
+    def _auth(*args, **kwargs):
+        cinema = args[0]
+        username = args[1]
+        if (not cinema.has_logged_user()) or cinema.user[DB_USERS_USERNAME_KEY] != username:
+            print('You have to be logged in as {u} to do that!'.format(u=username))
+            cinema.log_user_in()
+            if (not cinema.has_logged_user()) or cinema.user[DB_USERS_USERNAME_KEY] != username:
+                print('You have to be logged in as {u} to do that!'.format(u=username))
+                return
+    return _auth
