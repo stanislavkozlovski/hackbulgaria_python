e37489c256c56b92be571732b15b92a62ac67920
Enether <familyguyuser192@windowslive.com>
2017-02-15 16:05:44 +0200

    Added a decorator for a required teacher

--- week15_and_onwards/CourseManagementSystem/course_management_system_/accounts/decorators.py
+++ week15_and_onwards/CourseManagementSystem/course_management_system_/accounts/decorators.py
@@ -29,3 +29,17 @@ def login_required(redirect_url):
             return view_func(*args, **kwargs)
         return decorate
     return wrapper
+
+
+def teacher_required(redirect_url):
+    """ Requires a user to be logged in and for him to be a teacher"""
+    def wrapper(view_func):
+        def decorate(*args, **kwargs):
+            request: HttpRequest = args[0]
+
+            if 'user' not in request.session or not hasattr(request, 'user') or not request.user.is_teacher():
+                return redirect(redirect_url)
+
+            return view_func(*args, **kwargs)
+        return decorate
+    return wrapper
